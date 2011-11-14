00c2b40af5e73c5a56a960c89d77c725b46c46b9
Danilov Anatoly <danilovonline@gmail.com>
2011-11-14 22:59:34 +0400

    Change method 'interchange'. Use now core method 'clone'

--- JavaScript/MultySelect/multyselect.js
+++ JavaScript/MultySelect/multyselect.js
@@ -142,18 +142,4 @@ function multySelect(my_select, result_select) {
   	s.options[i1] = clone2;
     s.options[i2] = clone1;
   }
-
-  
-  function interchange1(s, i1, i2) {
-    t1 = s.options[i1].text;
-    v1 = s.options[i1].value;
-  
-    s.options[i1].text = s.options[i2].text;
-    s.options[i1].value = s.options[i2].value;
-
-    s.options[i2].text = t1;
-    s.options[i2].value = v1;
-  }
-
-
 }
